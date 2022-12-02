30a382f46b0eec2960480690161251f967967808
royce-mathew <royce1mathew@gmail.com>
2022-12-02 18:56:09 -0500

    Changed naming conventions for folders and also completed Day2 advent of code

---
+++ day2/target/debug/Day2.d
@@ -0,0 +1,1 @@
+/home/jaguar/Downloads/AdventOfCode2022/Day2/target/debug/Day2: /home/jaguar/Downloads/AdventOfCode2022/Day2/src/main.rs
---
+++ day2/target/debug/deps/Day2-5a1d478863409256.d
@@ -0,0 +1,5 @@
+/home/jaguar/Downloads/AdventOfCode2022/Day2/target/debug/deps/Day2-5a1d478863409256.rmeta: src/main.rs
+
+/home/jaguar/Downloads/AdventOfCode2022/Day2/target/debug/deps/Day2-5a1d478863409256.d: src/main.rs
+
+src/main.rs:
