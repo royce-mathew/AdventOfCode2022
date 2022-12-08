78bcd7a5d581adc5510a847bb0d1de64853a60eb
royce-mathew <royce1mathew@gmail.com>
2022-12-07 20:13:45 -0500

    Finished Day 5 - Crane Operation

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,26 +3,60 @@ use std::{io::{self, Write}, vec, fs};
 struct Solution;
 
 impl Solution {
-    fn part1(vector: &Vec<Vec<char>>) {
-
+    fn part1(vector: &mut Vec<Vec<char>>) {
         let read_str = fs::read_to_string("src/data.txt").unwrap_or("".to_owned());
         for line in read_str.split("\n") {
-            let mut parts: Vec<u32> = vec![];
+            let mut parts: Vec<usize> = vec![];
 
             line.split_whitespace()
-            .for_each(|x| match x.parse::<u32>() {
+            .for_each(|x| match x.parse::<usize>() {
                 Ok(value) => {
                     parts.push(value);
                 }
                 _ => {}
             });
 
-            // vec.Truncate
-            // vec.drain
+
+            // What index to start taking from
+            // Also its stated above it because we won't be able to access the vector in this scope after we take the reference in the next line
+            let starting_index = vector[parts[1] - 1].len() - parts[0];
+            let to_take_from = &mut vector[parts[1] - 1];
+            let elements = &mut to_take_from.drain(starting_index ..).collect::<Vec<_>>();
+
+            elements.reverse(); // Crates are supoposed to be put 1 by 1, this kind of simulates that
+
+            vector[parts[2] - 1].append(elements); // Append to end of the arg     
+        }
+    }
+
+    fn part2(vector: &mut Vec<Vec<char>>) {
+        let read_str = fs::read_to_string("src/data.txt").unwrap_or("".to_owned());
+        for line in read_str.split("\n") {
+            let mut parts: Vec<usize> = vec![];
+
+            line.split_whitespace()
+            .for_each(|x| match x.parse::<usize>() {
+                Ok(value) => {
+                    parts.push(value);
+                }
+                _ => {}
+            });
+
             
+
+
+            // What index to start taking from
+            // Also its stated above it because we won't be able to access the vector in this scope after we take the reference in the next line
+            let starting_index = vector[parts[1] - 1].len() - parts[0];
+            let to_take_from = &mut vector[parts[1] - 1];
+            let elements = &mut to_take_from.drain(starting_index ..).collect::<Vec<_>>();
+
+
+            vector[parts[2] - 1].append(elements); // Append to end of the arg   
             
-            
-            
+
+            Table::print_table(&vector);
+            println!("{}", line);
         }
     }
 }
@@ -63,7 +97,7 @@ impl Table {
             
         }
         print!(" ");
-        for i  in (1..=len+1) {
+        for i  in (1..=vector.len()) {
             print!("{}   ", i);
         }
         print!("\n");
@@ -75,7 +109,7 @@ impl Table {
 
 fn main() {
     // Create a stack for each row
-    let main_vector = vec![
+    let mut main_vector = vec![
         vec!['R', 'G', 'H', 'Q', 'S', 'B', 'T', 'N'],
         vec!['H', 'S', 'F', 'D', 'P', 'Z', 'J'],
         vec!['Z', 'H', 'V'],
@@ -87,6 +121,22 @@ fn main() {
         vec!['W', 'H', 'L', 'M', 'T', 'D', 'N', 'C']
     ];
 
+
+    // let mut test_vector = vec![
+    //     vec!['Z', 'N'],
+    //     vec!['M', 'C', 'D'],
+    //     vec!['P']
+    // ];
+
+
+    // Table::print_table(&test_vector);
+    // let j = Solution::part2(&mut test_vector);
+    // Table::print_table(&test_vector);
+
+
     Table::print_table(&main_vector);
-    let j = Solution::part1(&main_vector);
+    let j = Solution::part2(&mut main_vector);
+    Table::print_table(&main_vector);
+
+
 }
